7e43ebabe5cf7473dc05ab4a0893f500e34b7849
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-06 20:16:16 +0000

    adding the 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+B#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+int x;
+for (x = 0; x < 10; x++)
+{
+putchar(x);
+}
+return (0);
+}
