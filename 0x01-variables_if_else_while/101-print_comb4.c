01eabdc1d0640d7d424ff8c9456cf9ccbbfe956c
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-07 02:16:20 +0000

    adding the 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -12,30 +12,31 @@ int main(void)
 int x;
 int y;
 int z;
-  for (x = '0'; x <= '9'; x++)
-    {
-      for (y = '1'; y <= '9'; y++)
-	{
-	  for (z = '2'; z <= '9'; z++)
-	    {
-	  if (x != y && y != z)
-	    {
-	      if (x > y > z)
-		{
-		  continue;
-		}
-	      putchar(x);
-	      putchar(y);
-	      if (x == '7' && y == '8' && z == '9')
-		{
-		  break;
-		}
-	      putchar(',');
-	      putchar(' ');
-	    }
-	}
-}    
+for (x = '0'; x <= '7'; x++)
+{
+for (y = '1'; y <= '8'; y++)
+{
+for (z = '2'; z <= '9'; z++)
+{
+if (x != y && y != z && x != z)
+{
+if (x > y || y > z || x > z)
+{
+continue;
 }
-  putchar('\n');
-  return (0);
+putchar(x);
+putchar(y);
+putchar(z);
+if (x == '7' && y == '8' && z == '9')
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
