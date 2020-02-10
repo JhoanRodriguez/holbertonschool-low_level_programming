199d960c4b0f073bd83adfd9d1ae5e23a17ef341
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-10 20:42:39 +0000

    adding the comments for betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * print_alphabet_x10 - Print alphabet in lowercase using only `_putchar`
  * Return: 0
  */
 void print_alphabet_x10(void)
 {
 	int x;
 	int a;
+
 	for (a = 0 ; a < 10; a++)
 	{
 		for (x = 'a'; x <= 'z'; x++)
