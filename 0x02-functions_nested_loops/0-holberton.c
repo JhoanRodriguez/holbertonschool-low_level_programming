61245d2f2f7e2a2d72ad8f5b7f45cd18be0fa186
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-10 17:33:17 +0000

    updating the 0

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "holberton.h"
+/**
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
+ */
+int main(void)
+{
+	putchar (72);
+	putchar (111);
+	putchar (108);
+	putchar (98);
+	putchar (101);
+	putchar (114);
+	putchar (116);
+	putchar (111);
+	putchar (110);
+	putchar ('\n');
+	return (0);
+}
