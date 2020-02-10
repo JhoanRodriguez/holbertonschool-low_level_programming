0263a412ff6407940046ba5a0fd6ca92763853b8
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-10 20:14:33 +0000

    adding the 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		_putchar (x)
+			}
+	_putchar ('\n');
+}
