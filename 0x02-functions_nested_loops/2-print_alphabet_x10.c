ac0f092ec4150830337881fe3e03468475c97744
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-10 20:40:51 +0000

    adding the 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * print_alphabet - Print alphabet in lowercase using only `_putchar`
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int a;
+	for (a = 0 ; a < 10; a++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar (x);
+		}
+		_putchar ('\n');
+	}
+}
