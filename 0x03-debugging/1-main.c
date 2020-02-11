51d7e23f612a11593a6f9dba831b4ec189d0fa8a
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-11 19:06:59 +0000

    hotfix

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,20 +7,17 @@
 
 int main(void)
 {
-        int i;
+	/*int i;*/
 
-        printf("Infinite loop incoming :(\n");
+	printf("Infinite loop incoming :(\n");
 
-        i = 0;
+	/**
+	 * i = 0;
+	 * while (i < 10)
+	 * {  putchar(i);  }
+	 */
 
-        while (i < 10)
-        {
-                putchar(i);
-        }
-/**
- *the infinite loop is becase i is always 0
- */
-        printf("Infinite loop avoided! \\o/\n");
+	printf("Infinite loop avoided! \\o/\n");
 
-        return (0);
+	return (0);
 }
