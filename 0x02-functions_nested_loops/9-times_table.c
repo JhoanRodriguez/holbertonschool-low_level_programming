2eb3ed2d3f736a3bc0845c0c769bee21e8738e93
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-11 15:36:50 +0000

    fixing with betty

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,10 @@ void times_table(void)
 {
 	int x;
 	int y;
-	for (x = 0;x <= 9;x++)
+
+	for (x = 0; x <= 9; x++)
 	{
-		for (y = 0; y <= 9;y++)
+		for (y = 0; y <= 9; y++)
 		{
 			if ((x * y) < 10)
 			{
@@ -19,14 +20,14 @@ void times_table(void)
 				{
 				_putchar (' ');
 				}
-				_putchar ((x * y)+ '0');
+				_putchar ((x * y) + '0');
 			}
-			else 
+			else
 			{
 				_putchar (((x * y) / 10) + '0');
 				_putchar (((x * y) % 10) + '0');
 			}
-			if ( y != 9)
+			if (y != 9)
 			{
 			_putchar (',');
 			_putchar (' ');
