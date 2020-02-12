20eb1eca8e3685cd2888b420b5cf91748d7311a8
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-12 04:35:03 +0000

    fixing with betty

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include "holberton.h"
 /**
- * times_table - Show the multiplication tables
- * Return: Return nothing
+ * print_times_table - Show the multiplication tables
+ * @n: int type number
  */
 void print_times_table(int n)
 {
-	int x;
-	int y;
-	int z;
+	int x, y, z;
 
 	if (n >= 0 || n <= 15)
 	{
@@ -18,7 +15,6 @@ void print_times_table(int n)
 			for (y = 0; y <= n; y++)
 			{
 				z = x * y;
-
 				if (z < 10)
 				{
 					if (y != 0)
@@ -34,7 +30,7 @@ void print_times_table(int n)
 					_putchar ((z / 10) + '0');
 					_putchar ((z % 10) + '0');
 				}
-				else 
+				else
 				{
 					_putchar(z / 100 + '0');
 					_putchar((z / 10 % 10) + '0');
