c29baa9f7bb3006aeedcb1124c4474303a5c433e
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-13 16:13:00 +0000

    adding the 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+* mul - Print the last digit
+* @x: Number that is going to be splited
+* @y: Number that is going to be splited
+* Return: Return the last digit
+*/
+int mul(int x, int y)
+{
+	return (x * y);
+}
