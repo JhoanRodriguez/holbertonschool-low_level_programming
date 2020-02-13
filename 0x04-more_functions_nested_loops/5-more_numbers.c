e16651506323051c401b35eaee7324ab69fced9c
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-13 17:39:00 +0000

    adding the 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * more_numbers - Print the last digit
+ * Return: Return the last digit
+ */
+void more_numbers(void)
+{
+	int x;
+
+	for (x = 0; x <= 14; x++)
+	{
+		_putchar (x / 10);
+		if (x > 9)
+		{
+			_putchar (x % 10);
+		}
+	}
+	_putchar ('\n');
+}
