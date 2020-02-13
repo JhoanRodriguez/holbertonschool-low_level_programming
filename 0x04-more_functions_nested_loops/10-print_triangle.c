e6d10b09951021e829134d23d33f01146f64cd95
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-13 22:23:47 +0000

    adding the 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * print_triangle - Print the last digit
+ * @size: Number that is going to be splited
+ * Return: Return the last digit
+ */
+void print_triangle(int size)
+{
+	int x, y, z;
+
+	for (x = 0; (x < size); x++)
+	{
+		for (y = 0; y < (size - x - 1); y++)
+		{
+			_putchar (' ');
+		}
+		for (z = 0; z <= x; z++)
+		{
+			_putchar ('#');
+		}
+		_putchar ('\n');
+	}
+}
