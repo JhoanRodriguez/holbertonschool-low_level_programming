ccd91dd25ece7419ff9de0c276311940f8aec268
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-13 20:34:40 +0000

    adding the 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_square - Print the last digit
+ * @zize: Number that is going to be splited
+ * Return: Return the last digit
+ */
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar ('\n');
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar ('#');
+			}
+			_putchar ('\n');
+		}	
+	}
+}
