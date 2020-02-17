bfcb5983e99154c47b38c9f3bca528e9cb3d7299
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-17 18:34:59 +0000

    adding the 1 and fixing the betty for the 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 /**
- * _reset_to_98 - Print the last digit
- * @x: Number that is going to be splited
+ * reset_to_98 - Print the last digit
+ * @n: Number that is going to be splited
  * Return: Return the last digit
  */
 void reset_to_98(int *n)
