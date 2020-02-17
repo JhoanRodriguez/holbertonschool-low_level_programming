87bc79770cd0a948d112eff5825a9f431fd63761
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-17 18:27:27 +0000

    adding the solution for the 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "holberton.h"
+/**
+ * _reset_to_98 - Print the last digit
+ * @x: Number that is going to be splited
+ * Return: Return the last digit
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
