35fec44a1e2a01b048105bb972d5770fa7977c47
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-19 00:35:33 +0000

    adding the 100

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,3 +7,30 @@
  */
 
 int _atoi(char *s)
+{
+	int c = 0;
+	int x;
+	int signo = 0;
+	int num = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	for (x = 0; x < c; x++)
+	{
+		if (s[x] == '-')
+		{
+			signo = -1;
+		}
+		if ( s[x] >= 0 && s[x] <= 9)
+		{
+			num = num + s[x] + '0';
+		}
+	}
+	if (signo != 0)
+	{
+		num = num * signo;
+	}
+	return (num);
+}
