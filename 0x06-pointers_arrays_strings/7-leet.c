8ea0a40932be0e063ff1007c28ed55de10d725dd
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-20 23:52:59 +0000

    fixing the 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,29 +9,18 @@
 char *leet(char *s)
 {
 	int x;
-
-	for (x = 0; s[x] != '\0'; x++)
+	int y;
+	char A[] = "aeotlAEOTL";
+	char N[] = "4307143071";
+	
+	for (x = 0; s[x] != 0; x++)
 	{
-		while (s[x] == 'a' || s[x] == 'A')
+		for (y = 0; A[y] != '\0'; y++)
 		{
-			s[x] = '4';
+			if (s[x] == A[y])
+			{
+				s[x] = N[y];
+			}
 		}
-		while (s[x] == 'e' || s[x] == 'E')
-		{
-			s[x] = '3';
-		}
-		while (s[x] == 'o' || s[x] == 'O')
-		{
-			s[x] = '0';
-		}
-		while (s[x] == 't' || s[x] == 'T')
-		{
-			s[x] = '7';
-		}
-		while (s[x] == 'l' || s[x] == 'L')
-		{
-			s[x] = '1';
-		}
-	}
-	return (s);
+	}return (s);
 }
