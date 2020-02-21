80ab2382f52537d0cb5da3edd81a1517b2d844a6
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-21 01:18:41 +0000

    adding the 8

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -8,5 +8,20 @@
 
 char *rot13(char *s)
 {
+	int x;
+	int y;
+	char A[] = "abcdefghijklmABCDEFGHIJKLM";
+	char N[] = "nopqrstuvwxyzNOPQRSTUVWXYZ";
 
+	for (x = 0; s[x] != 0; x++)
+	{
+		for (y = 0; A[y] != '\0'; y++)
+		{
+			if (s[x] == A[y])
+			{
+				s[x] = N[y];
+			}
+		}
+	}
+	return (s);
 }
