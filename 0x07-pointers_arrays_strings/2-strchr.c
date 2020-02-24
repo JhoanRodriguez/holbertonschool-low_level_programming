2b015d5c6aef5c7d98869e5749e850dec920d1c3
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-24 17:20:30 +0000

    adding the 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * _strchr - locate character in a string
+ * @s: char array string
+ * @c: char to look for
+ * Return: NULL if char not found, or pointer to first occurrence of char `c`
+ */
+
+char *_strchr(char *s, char c)
+{
+
+	if (s == '\0')
+		return (s);
+
+	for (; s != '\0'; s++)
+	{
+		if (*s == c)
+		{
+			break;
+		}
+	}
+	return (s);
+}
