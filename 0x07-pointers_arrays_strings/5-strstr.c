48516dc8f435dd378e16b23a01d110e986e46406
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-25 16:13:09 +0000

    adding the 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -3,31 +3,31 @@
 
 /**
  * _strstr - locate a substring
- * @haystack: string
- * @needle: string substring
+ * @h: string
+ * @n: string substring
  * Return: Pointer to beginning of substring, or NULL if not found.
  */
 
 char *_strstr(char *h, char *n)
 {
-	int x, c = 0;
-	char **y = 0;
+	int x, y, z;
 
 	if (h == NULL || n == NULL)
 		return (0);
 
-	for (; *h != '\0'; h++)
+	for (x = 0; h[x] != '\0'; x++)
 	{
-		for (x = 0; *(n + x) != '\0'; x++)
+		for (y = z , j = 0; n[j] != '\0';j++, z++)
 		{
-			if (*h == n[x])
+			if (h[pos] != n[j] || h[z] == '\0')
 			{
-				c++;
-			}
-			else if (*h == '\0')
-			{
-				return (NULL);
+				break;
 			}
 		}
+		if (n[j] == 0)
+		{
+			return (&h[x]);
+		}
 	}
+	return (0);
 }
