67bfed74dc84941693a6fc91edee27814a1c55f7
JhoanRodriguez <jhoansebastian@msn.com>
2020-02-28 19:06:34 +0000

    adding the 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - print all arguments it receives
+ * @argc: argument count
+ * @argv: argument vector, array of strings
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+	{
+		printf("%s\n", argv[x]);
+	}
+	return (0);
+}
