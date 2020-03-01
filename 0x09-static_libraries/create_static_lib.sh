78db943c60b455c83c956d87d8e50502a2885d03
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-01 16:37:33 +0000

    adding git ignore

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-<<<<<<< HEAD
 gcc -Wall -pedantic -Werror -Wextra -c *.c
 ar -rc liball.a *.o
 ranlib liball.a
-=======
-gcc -c *.c
-ar rcs liball.a *.o
->>>>>>> 114779a629a039ca5b444a276578052c5b136f69
