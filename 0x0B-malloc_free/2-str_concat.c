5b83f688293418837a3aa966c3604159beb19e05
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-03 21:52:39 +0000

    fixing the 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,79 +1,68 @@
 #include "holberton.h"
 #include <stdlib.h>
-char *_strncat(char *dest, char *src, int n);
-int _strlen(char *s);
+#include <stdio.h>
 /**
- * str_concat - concatenate two strings using malloc
- * @s1: string 1
- * @s2: string 2
- * Return: pointer to concat string
+ * _strlen - return length of a string
+ *
+ * @s: char type
+ * Return:  length of string
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+		;
+	return (a);
+}
+/**
+ * _strncat - function to concatnate strings with n bytes
+ *
+ * @dest: destination for concatnation
+ * @src: source of string
+ * @n: int type for size of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len, a;
+
+	dest_len = _strlen(dest);
+	for (a = 0; a < n && src[a] != '\0'; a++)
+		dest[dest_len + a] = src[a];
+	return (dest);
+}
+
+/**
+ * *str_concat - function to allocate space for sting concatnation
+ * @s1: array pointer to destination of string
+ * @s2: array pointer to source of string
+ * Return: return pointer to copy of string
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int a, b;
-	char *s3;
+	char *ptr;
+	int size1, size2;
 
 	if (s1 == NULL)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
 		s2 = "";
 	}
-	a = _strlen(s1);
-	b = _strlen(s2);
+	size1 = _strlen(s1);
+	size2 = _strlen(s2);
 
-	s3 = malloc(((a + b) + 1)* sizeof(char));
-
-	if (s3 == NULL)
+	ptr = malloc(((size1 + size2) + 1) * sizeof(char));
+	if (ptr == NULL)
 	{
-		return (0);
+		return (NULL);
 	}
-	_strncat(s3,s1,a);
-	_strncat(s3,s2,b);
-	s3[a+b+1] = '\0';
-	return (s3);
-}
-/**
- * _strlen - Print the last digit
- * @s: Number that is going to be splited
- * Return: Return the last digit
- */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-	return (c);
-}
-/**
- * _strncat - concatenate two strings
- * @dest: char string to concatenate to
- * @src: char string
- * @n: char to check
- * Return: 'dest'
- */
-
-char *_strncat(char *dest, char *src, int n)
-{
-	int a, b;
-
-	a = 0;
-	while (dest[a] != '\0')
-		a++;
-
-	for (b = 0; src[b] != 0; b++)
-	{
-		if (b < n)
-			dest[a] = src[b];
-
-		++a;
-	}
-
-	return (dest);
+	_strncat(ptr, s1, size1);
+	_strncat(ptr, s2, size2);
+	ptr += '\0';
+	return (ptr);
 }
