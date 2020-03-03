c96d6ec1b4d84f2302cbf66f8e3d969045634fa1
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-03 19:27:18 +0000

    adding the 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+#include <stdlib.h>
+int _strlen(char *s);
+/**
+ * str_concat - concatenate two strings using malloc
+ * @s1: string 1
+ * @s2: string 2
+ * Return: pointer to concat string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int x, y = 0;
+	char *s3 = malloc(_strlen(s1) + _strlen(s2));
+
+	if (s1 == NULL || s2 == NULL)
+	{
+		return (0);
+	}
+	if (s3 == NULL)
+	{
+		return (0);
+	}
+	for (x = 0; x < _strlen(s1); x++)
+	{
+		s3[x] = s1[x];
+	}
+	for (; x < (_strlen(s1) + _strlen(s2)); x++, y++)
+	{
+		s3[x] = s2[y];
+	}
+	return (s3);
+}
+/**
+ * _strlen - Print the last digit
+ * @s: Number that is going to be splited
+ * Return: Return the last digit
+ */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	while (s[c] != '\0')
+	{
+		c++;
+	}
+	return (c);
+}
