9a3c14fbb034601af1445ec2dcc60c9a58ae2ea9
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-04 01:29:47 +0000

    fixing the 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 	int x, y;
 	int **matrix;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0 || height <= 0 || width == NULL || height == NULL)
 	{
 		return (0);
 	}
