7e3889fa242f7853851a6b536bfc3ea99e54d9e8
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-09 21:46:56 +0000

    adding the 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef dog_h
 #define dog_h
+#include <stdio.h>
+#include <stdlib.h>
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 /**
  * struct dog - structure definition of a dog
  * @name: character string
