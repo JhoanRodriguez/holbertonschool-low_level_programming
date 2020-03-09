5fb972a2b195d73d87d06289db402adf5794b40e
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-09 19:08:37 +0000

    adding the 2

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdlib.h>
+/**
+ * main - Print the name of the file compiled from
+ * Return: 0;
+ */
+
+int main(void)
+{
+	printf ("s\n", _FILE_);
+	return (0);
+}
