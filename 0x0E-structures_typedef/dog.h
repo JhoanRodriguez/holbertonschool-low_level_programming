f8209d8844015ceb1588bbbb319cf09a840bacfd
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-09 21:15:31 +0000

    addint the dog file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: integer
+ * @owner: character string
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
