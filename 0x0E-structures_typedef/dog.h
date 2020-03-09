3ba012a33e2da6542f9881408caebc181c502c4a
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-09 21:51:32 +0000

    fixing the 1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,6 @@
 #define dog_h
 #include <stdio.h>
 #include <stdlib.h>
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 /**
  * struct dog - structure definition of a dog
  * @name: character string
@@ -18,4 +14,8 @@ struct dog
 	float age;
 	char *owner;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
