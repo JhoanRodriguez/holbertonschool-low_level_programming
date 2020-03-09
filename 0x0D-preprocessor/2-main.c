931805af4faf54aefbfa79253b917761e78eb8dd
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-09 19:10:13 +0000

    adding the 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <stdlib.h>
+#include <stdio.h>
 /**
  * main - Print the name of the file compiled from
  * Return: 0;
@@ -7,6 +7,6 @@
 
 int main(void)
 {
-	printf ("s\n", _FILE_);
+	printf("s\n", _FILE_);
 	return (0);
 }
