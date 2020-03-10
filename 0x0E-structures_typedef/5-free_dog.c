aa34faa6290bfb1b1baa14e6066b84d26677b10f
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-10 01:36:41 +0000

    adding the 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - free the dogs
+ * @d: pointer to dog struct
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	{
+		return (0);
+	}
+	else
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
