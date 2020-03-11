56eb54ba8b11e8ff9375de6801c5bc75c2004a5f
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-11 23:01:01 +0000

    adding the 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * get_op_func - select the correct operation function asked by user
+ * @s: operator argument
+ * Return: function pointer corresponding to operator given
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int x = 0;
+
+	while (x < 5)
+	{
+		if (*(ops[x].op) == *s)
+		{
+			return (ops[x].f);
+		}
+		x++;
+	}
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,30 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: string of arguments in array
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	else
+	{
+		printf("%i\n",get_op_func(argv[2])(atoi(argv[1]),atoi(argv[3])));
+	}
+	return (0);
+}
