a793fcd9a0cb95dde55265fa07dbced04c4c34f8
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-11 19:01:41 +0000

    fixing the 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,5 +9,13 @@
 
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned long x;
 
+	if (!array || !size || !action)
+		return;
+
+	for (x = 0; x < size; x++)
+	{
+		action(array[x]);
+	}
 }
