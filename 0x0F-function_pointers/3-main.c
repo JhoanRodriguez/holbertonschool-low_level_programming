3e6f3d264a97053d97f1039154952029ff41bb4b
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-11 23:25:06 +0000

    fixing the 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -18,15 +18,19 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
+	if (argv[2][1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
 	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-      
-	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])) ;
+
+	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
 	printf("%i\n", result);
-	
+
 	return (0);
 }
