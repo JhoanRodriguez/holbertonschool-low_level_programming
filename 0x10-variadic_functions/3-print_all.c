d30610aa96c3d3d32096dff9796bdfedba3017dd
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-14 20:26:43 +0000

    fixing the 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -2,21 +2,33 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * _printchar - print char type element from va_list
+ * print_char - print char type element from va_list
  * @list: va_list passed to function
  */
 void print_char(va_list list)
 {
 	printf("%c, ", va_arg(list, int));
 }
+/**
+ * print_int - print int type element from va_list
+ * @list: va_list passed to function
+ */
 void print_int(va_list list)
 {
 	printf("%d, ", va_arg(list, int));
 }
+/**
+ * print_float - print float type element from va_list
+ * @list: va_list passed to function
+ */
 void print_float(va_list list)
 {
 	printf("%f, ", va_arg(list, double));
 }
+/**
+ * print_string - print string type element from va_list
+ * @list: va_list passed to function
+ */
 void print_string(va_list list)
 {
 	char *x;
@@ -27,6 +39,10 @@ void print_string(va_list list)
 	else
 		printf("(nil)");
 }
+/**
+ * print_all - print all type element from va_list
+ * @format: va_list passed to function
+ */
 void print_all(const char * const format, ...)
 {
 	int x = 0, y = 0;
