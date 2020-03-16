c863935cb26d9c724b8c473c9d2c1d16f1ba15ad
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-16 23:19:23 +0000

    fixing with betty the variadic.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,9 +6,8 @@ void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
 /**
- * struct datatype - Struct datatype
- *
- * @datatype: The operator
+ * struct data_type - Struct datatype
+ * @type: The operator
  * @f: The function associated
  */
 typedef struct data_type
