7b785eb751d1219ad87d0c73b31a481933eac52e
JhoanRodriguez <jhoansebastian@msn.com>
2020-03-26 22:35:43 +0000

    fixing the 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,7 +11,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned int index = 0;
 	unsigned long int counter;
 
-	flip = n ^ m;
+	counter = n ^ m;
 	while (counter)
 	{
 		if ((cunter & 1) == 1)
