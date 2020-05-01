a8ee9449dfd5f7ba1b604a60ae4dba8b7d0f7b0c
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-01 21:51:37 +0000

    adding the 5

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - get the nth node of a `dlistint_t` doubly LL
+ * @head: pointer to head of LL
+ * @index: node index to return
+ * Return: node at index given or NULL if node does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int counter = 0;
+
+	while (head != NULL && counter < index)
+	{
+		head = head->next;
+		counter ++;
+	}
+	return (head);
+}
