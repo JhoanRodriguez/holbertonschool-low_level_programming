ef4d9159edc8c6bd6e2ef99151c0173d1f248698
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-04 16:07:30 +0000

    Adding the basic files

---
+++ 0x18-dynamic_libraries/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _isupper - Print alphabet in lowercase 10 times
+ * Return: 0
+ * @x: char type letter
+ */
+int _isupper(int x)
+{
+	if (x >= 65 && x <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
