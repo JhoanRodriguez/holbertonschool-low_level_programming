7b7017738f37db6705baec43b1970244da62ee1d
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-29 15:12:07 +0000

    fixing with betty

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,7 @@
 #include "hash_tables.h"
 
-hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,const char * value, const char *key);
+hash_node_t *insert_node(hash_node_t **head,
+						unsigned long int idx, const char *value, const char *key);
 
 /**
  * hash_table_set - Adds an element to the hash table
@@ -11,24 +12,26 @@ hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,c
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    unsigned long int index;
+	unsigned long int index;
 
-    if (!key || strcmp(key, "") == 0 || !ht || !value)
-        return (0);
-    
-    index = key_index((unsigned char *)key, ht->size);
-    insert_nodeint_at_index(ht->array, index, value, key);
+	if (!key || strcmp(key, "") == 0 || !ht || !value)
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+	insert_node(ht->array, index, value, key);
 	return (1);
 
 }
 /**
- * insert_nodeint_at_index - function to insert a node in a list at given index
+ * insert_node - function to insert a node in a list at given index
  * @head: pointer to the pointer to list
  * @idx: given index to insert node at
- * @n: data to be inserted
+ * @value: data to be inserted
+ * @key: key
  * Return: the address of new node inserted or NULL
  */
-hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,const char *value, const char *key)
+hash_node_t *insert_node(hash_node_t **head,
+						unsigned long int idx, const char *value, const char *key)
 {
 	unsigned int i;
 	hash_node_t *next_node, *prev_node, *new_node;
@@ -48,9 +51,9 @@ hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,c
 	if (!new_node)
 		return (NULL);
 
-    new_node->key  = strdup(key);
-    new_node->value = strdup(value);
-    
+	new_node->key  = strdup(key);
+	new_node->value = strdup(value);
+
 	if (idx == 0)
 	{
 		new_node->next = *head;
