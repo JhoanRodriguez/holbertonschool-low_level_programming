09cb6a815d7928d6c8b040f359bd6c7907d024b4
JhoanRodriguez <jhoansebastian@msn.com>
2020-05-29 14:59:15 +0000

    adding the 3 - 5

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,73 @@
+#include "hash_tables.h"
+
+hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,const char * value, const char *key);
+
+/**
+ * hash_table_set - Adds an element to the hash table
+ * @ht: hash table to add to
+ * @key: key to add
+ * @value: value to store for key
+ * Return: 1 if success, 0 if fail
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+    unsigned long int index;
+
+    if (!key || strcmp(key, "") == 0 || !ht || !value)
+        return (0);
+    
+    index = key_index((unsigned char *)key, ht->size);
+    insert_nodeint_at_index(ht->array, index, value, key);
+	return (1);
+
+}
+/**
+ * insert_nodeint_at_index - function to insert a node in a list at given index
+ * @head: pointer to the pointer to list
+ * @idx: given index to insert node at
+ * @n: data to be inserted
+ * Return: the address of new node inserted or NULL
+ */
+hash_node_t *insert_nodeint_at_index(hash_node_t **head, unsigned long int idx,const char *value, const char *key)
+{
+	unsigned int i;
+	hash_node_t *next_node, *prev_node, *new_node;
+
+	next_node = prev_node =  *head;
+	i = 0;
+
+	while (next_node)
+	{
+		i++;
+		next_node = next_node->next;
+	}
+	if (idx > i)
+		return (NULL);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (!new_node)
+		return (NULL);
+
+    new_node->key  = strdup(key);
+    new_node->value = strdup(value);
+    
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
+
+	next_node = *head;
+	new_node->next = NULL;
+
+	for (i = 0; i < idx; i++)
+		next_node = next_node->next;
+
+	for (i = 0; i < idx - 1; i++)
+		prev_node = prev_node->next;
+
+	new_node->next = next_node;
+	prev_node->next = new_node;
+	return (new_node);
+}
---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - Print a hash table
+ * @ht: hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int x;
+    hash_node_t *tmp;
+
+    for (x = 0; x < ht->size; x++)
+    {
+        tmp = ht->array[x];
+        if (tmp != NULL)
+        {
+            printf("'%s': '%s'", tmp->key, tmp->value);
+            while ((tmp = tmp->next) != NULL)
+			{
+				printf(", ");
+				printf("'%s': '%s'", tmp->key, tmp->value);
+			}
+        }
+    }
+}
