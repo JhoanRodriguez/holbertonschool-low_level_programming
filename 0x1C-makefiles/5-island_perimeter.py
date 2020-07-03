8c45349c75d5cd9301521a951d1167c4b153c1b7
JhoanRodriguez <jhoansebastian@msn.com>
2020-07-03 22:49:53 +0000

    Adding the 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,3 +1,16 @@
 #!/usr/bin/python3
+"""Calculate the perimeter of and island
+    """
+
 
 def island_perimeter(grid):
+    """Return the perimeter of the island
+    """
+    counter = 0
+    for item in grid:
+        for land in item:
+            if land == 1:
+                counter += 1
+    per = (counter - 2) * 2
+    per += (2 * 3)
+    return per
