a6618b64c94df29bbdb11e138ce106b59060098a
JhoanRodriguez <jhoansebastian@msn.com>
2020-07-04 00:20:21 +0000

    Fixing the edges

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,12 @@ def island_perimeter(grid):
         for y in range(0, len(grid[0])):
             if (grid[x][y] == 1):
                 counter += 4
-                if grid[x - 1][y] == 1:
+                if x - 1 >= 0 and grid[x - 1][y] == 1:
                     counter -= 1
-                if grid[x + 1][y] == 1:
+                if x + 1 < len(grid) and grid[x + 1][y] == 1:
                     counter -= 1
-                if grid[x][y - 1] == 1:
+                if y - 1 >= 0 and grid[x][y - 1] == 1:
                     counter -= 1
-                if grid[x][y + 1] == 1:
+                if y + 1 < len(grid[0]) and grid[x][y + 1] == 1:
                     counter -= 1
     return counter
