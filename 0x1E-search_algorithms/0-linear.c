b2c9913d81e66b85cd135f96237a5b3e7f6c3c52
JhoanRodriguez <jhoansebastian@msn.com>
2020-08-26 04:21:24 +0000

    Adding the 1 and fixed with betty

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -10,11 +10,12 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t x = 0;
+
 	if (array)
 	{
-		for (x = 0;x < size; x++)
+		for (x = 0; x < size; x++)
 		{
-			printf("Value checked array[%lu] = [%d]\n",x , array[x]);
+			printf("Value checked array[%lu] = [%d]\n", x, array[x]);
 			if (array[x] == value)
 				return (x);
 		}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,51 @@
+#include "search_algos.h"
+/**
+ * binary_search - function to print the array where the value will be searched
+ * @array: array to be searched
+ * @left: left value
+ * @right: right value
+ */
+void print_array(int *array, size_t left, size_t right)
+{
+	printf("Searching in array: ");
+
+	for (; left <= right; left++)
+	{
+		printf("%d", array[left]);
+		if (left != right)
+			printf(", ");
+	}
+	printf("\n");
+}
+
+
+/**
+ * binary_search - function to do binary search
+ * @array: array to be searched
+ * @size: size of array
+ * @value: value to be searched
+ * Return: -1 else index
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	size_t right = 0, left = 0, middle = 0;
+
+	if (array)
+	{
+		right = size - 1;
+
+		while (left <= right)
+		{
+			print_array(array, left, right);
+			middle = (left + right) / 2;
+			if (array[middle] == value)
+				return (middle);
+			else if (array[middle] < value)
+				left = middle + 1;
+			else
+				right =  middle - 1;
+		}
+	}
+
+	return (-1);
+}
